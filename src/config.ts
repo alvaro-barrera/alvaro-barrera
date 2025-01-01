f8082b970a0d8acf3a771c6ec3594f14342cac7c
alvaro-barrera <abarrera@buk.pe>
2025-01-01 01:20:24 -0500

    chore: optimize images

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export const SITE: Site = {
   desc: "Álvaro Barrera, software engineer en proyectos con Ruby on Rails, Laravel, desarrollo de API's, facturación electrónica, Single Page Application, conocimientos en marcos de trabajo ágiles - SCRUM",
   title: "Álvaro Barrera | Desarrollador de software",
   ogImage: "alvarobarrera.png",
-  lightAndDarkMode: true,
+  lightAndDarkMode: false,
   postPerIndex: 4,
   postPerPage: 3,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
